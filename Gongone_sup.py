d33003340f9dd964e764b10fec50e6680eade8f5
Choi Kang <choikang0308@gmail.com>
2022-02-08 08:57:14 +0900

    Add Sup to Chat_Banning

--- Gongone_sup.py
+++ Gongone_sup.py
@@ -15,17 +15,19 @@ async def on_ready():
 
 @bot.event
 async def on_message(ctx):
+  if ctx.author.id == bot.user.id:
+    return
+
   if ctx.channel.id == 937561123912306758:
     if ctx.content[0] == "!":
       await ctx.channel.send("전적이는 여기서! <#934776289884262441>", reference=ctx)
   
-  ban_words = ['시발', 'ㅅㅂ']
+  ban_words = ["시발", "ㅅㅂ", "존나", "ㅈㄴ", "좆", "ㅈ같", "애미", "개새끼"]
   for i in range(len(ban_words)):
     for j in range(len(ctx.content)-len(ban_words[i])+1):
-      if ctx.content[j:j+len(ban_words[i])-1] == ban_words[i]:
-        if ctx.channel.id == 940161168486567957 or ctx.channel.id == 939841339816820766:
-          return
-        await ctx.channel.send("욕ㄴㄴ", reference=ctx)
+      if ctx.content[j:j+len(ban_words[i])] == ban_words[i]:
+        await ctx.delete()
+        await ctx.channel.send(f"{ctx.author.mention} 부적절한 단어를 사용했어요..!")
         return
 
 
