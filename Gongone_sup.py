eac836fa3c31d083634b792594fd53760b9f6eba
Choi Kang <choikang0308@gmail.com>
2022-02-10 21:48:39 +0900

    Sup Savefile to yaml

--- Gongone_sup.py
+++ Gongone_sup.py
@@ -1,28 +1,31 @@
 import discord
 from discord.ext import commands
-from keep_alive import keep_alive
-import os
+import Project_manager
 
 
 bot = commands.Bot(command_prefix=';;',
                    status=discord.Status.online, intents=discord.Intents.all())
 
+pm_data = Project_manager.data()
+
 
 @bot.event
 async def on_ready():
-  print(f'Gongone.Service: Pay Script Runned on [{bot.user}]')
+  print(f'Gongone.Service: Sup Script Runned on [{bot.user}]')
 
 
 @bot.event
 async def on_message(ctx):
+  await bot.process_commands(ctx)
+
   if ctx.author.id == bot.user.id:
     return
 
   if ctx.channel.id == 937561123912306758:
     if ctx.content[0] == "!":
       await ctx.channel.send("전적이는 여기서! <#934776289884262441>", reference=ctx)
-  
-  ban_words = ["시발", "ㅅㅂ", "존나", "ㅈㄴ", "좆", "ㅈ같", "애미", "개새끼"]
+
+  ban_words = pm_data.secrets_get("ban_words")
   for i in range(len(ban_words)):
     for j in range(len(ctx.content)-len(ban_words[i])+1):
       if ctx.content[j:j+len(ban_words[i])] == ban_words[i]:
@@ -31,8 +34,13 @@ async def on_message(ctx):
         return
 
 
+@bot.command()
+async def join(ctx):
+  if ctx.author.voice and ctx.author.voice.channel:
+    channel = ctx.author.voice.channel
+    await channel.connect()
+  else:
+    await ctx.send("채널에 연결되지 않았습니다.")
 
-keep_alive()
 
-
-bot.run(os.environ["token"])
+bot.run(pm_data.secrets_get("tokens")["Gongone_sup"])
